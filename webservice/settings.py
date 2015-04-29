94fa3f0cd4cc04b4b54420bbfbdda248f0b7da12
Chris Weber <chrisr.weber@gmail.com>
2015-04-29 07:57:04 +0200

    clean up settings.py and added requirements with requirements.txt

--- webservice/settings.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-WEBSERVICE = getattr(settings, 'WEBSERVICE', {
-    'IMAGE_SERVICE_TOKEN': "demo",
-    'IMAGE_SERVICE_URL': "http://127.0.0.1:5000/images/demo_project/",
-    'ANONYMOUS_USERNAME': "webservice_user"
-})
